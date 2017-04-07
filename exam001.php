62659140edcc71bed044e89d47b95b43ac6e8eeb
shinya-nexseed <shirai@nexseed.net>
2017-04-07 20:34:15 +0800

    Initial commit

---
+++ exam001.php
@@ -0,0 +1,13 @@
+<?php
+    // 下記プログラムのError, Notice, Warningをすべて解決せよ
+
+    // ①
+    $var1 = 'hoge';
+    echo 'hoge';
+    echo $var1
+    echo 'hoge';
+    echo var1;
+
+    // ②
+    $arr1 = array('hoge1','hoge2','hoge3','hoge4','hoge5');
+?>
